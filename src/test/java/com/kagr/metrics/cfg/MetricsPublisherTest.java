824d25b8bf64f8a12ce43e15a2b7a75cab53d1c3
... <...>
2021-09-21 10:08:09 -0400

    udpates

---
+++ src/test/java/com/kagr/metrics/cfg/MetricsPublisherTest.java
@@ -0,0 +1,61 @@
+/****************************************************************************
+ * FILE: MetricsPublisherTest.java
+ * DSCRPT: 
+ ****************************************************************************/
+
+package com.kagr.metrics.cfg;
+
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.junit.Test;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
+
+@Slf4j
+public class MetricsPublisherTest
+{
+
+    @Test
+    public void test()
+    {
+        try
+        {
+            final Parameters params = new Parameters();
+            final FileBasedConfigurationBuilder<XMLConfiguration> builder = new FileBasedConfigurationBuilder<XMLConfiguration>(XMLConfiguration.class)
+                    .configure(params.xml()
+                            .setThrowExceptionOnMissing(false)
+                            .setEncoding("UTF-8")
+                            .setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
+                            .setValidating(false)
+                            .setFileName("./src/test/resources/junit-metrics.xml"));
+            final XMLConfiguration config = builder.getConfiguration();
+            AppMetrics metrics = AppMetrics.initFromConfig(config);
+
+
+            try
+            {
+                Thread.sleep(1000000000);
+            }
+            catch (InterruptedException ex_)
+            {
+                _logger.error(ex_.toString(), ex_);
+            }
+        }
+        catch (ConfigurationException ex_)
+        {
+            _logger.error(ex_.toString(), ex_);
+        }
+
+
+    }
+
+}
