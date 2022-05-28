785f251b67be968ec504b67b2ce3a016de4a5627
Kevin A. Goldstein R. <kevgol0@gmail.com>
2022-05-27 23:25:56 -0700

    added hostname as a tag

--- src/main/java/com/kagr/metrics/cfg/AppMetrics.java
+++ src/main/java/com/kagr/metrics/cfg/AppMetrics.java
@@ -13,6 +13,8 @@ package com.kagr.metrics.cfg;
 
 
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -31,6 +33,7 @@ import org.jasypt.util.text.BasicTextEncryptor;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.binder.jvm.JvmCompilationMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
@@ -45,6 +48,7 @@ import io.micrometer.influx.InfluxMeterRegistry;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.experimental.var;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -56,383 +60,402 @@ import lombok.extern.slf4j.Slf4j;
 public class AppMetrics
 {
 
-    private static final long serialVersionUID = 1741838256855050257L;
+	private static final long serialVersionUID = 1741838256855050257L;
 
 
-    @Getter private CompositeMeterRegistry _registry;
+	@Getter
+	private CompositeMeterRegistry _registry;
 
-    private String _appName;
+	private String _appName;
 
-    private String _dbName;
-    private String _dbUsername;
-    private String _dbPassword;
-    private String _uri;
+	private String	_dbName;
+	private String	_dbUsername;
+	private String	_dbPassword;
+	private String	_uri;
 
-    private boolean _autoCreateDb;
-    private boolean _enabled;
+	private String _hostname;
 
-    private int _batchSize;
-    private int _nPublisherThreads;
-    private int _reportingFrequencyInSeconds;
+	private boolean	_autoCreateDb;
+	private boolean	_enabled;
 
+	private int	_batchSize;
+	private int	_nPublisherThreads;
+	private int	_reportingFrequencyInSeconds;
 
 
+	private Tags _commonTags;
 
 
-    private AppMetrics()
-    {
-        try
-        {
-            _registry = new CompositeMeterRegistry(Clock.SYSTEM);
-        }
-        catch (Exception ex_)
-        {
-            _logger.error(ex_.toString(), ex_);
-            throw new RegistryNotReadyException("AppMetrics::AppMetrics() - registy not ready!");
-        }
-    }
 
 
 
+	private AppMetrics()
+	{
+		try
+		{
+			_registry = new CompositeMeterRegistry(Clock.SYSTEM);
+		}
+		catch (Exception ex_)
+		{
+			_logger.error(ex_.toString(), ex_);
+			throw new RegistryNotReadyException("AppMetrics::AppMetrics() - registy not ready!");
+		}
 
 
-    public static final AppMetrics initFromConfig(String cfg_)
-    {
-        try
-        {
-            _logger.info("loading config from:{}", cfg_);
-            final Parameters params = new Parameters();
-            final FileBasedConfigurationBuilder<XMLConfiguration> builder = new FileBasedConfigurationBuilder<XMLConfiguration>(XMLConfiguration.class)
-                    .configure(params.xml()
-                            .setThrowExceptionOnMissing(false)
-                            .setEncoding("UTF-8")
-                            .setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
-                            .setValidating(false)
-                            .setFileName(cfg_));
-            final XMLConfiguration config = builder.getConfiguration();
-            return initFromConfig(config);
-        }
-        catch (ConfigurationException ex_)
-        {
-            _logger.error(ex_.toString(), ex_);
-        }
 
-        return null;
-    }
+		try
+		{
+			_hostname = InetAddress.getLocalHost().getHostName();
+			_commonTags = Tags.of("hostname", _hostname);
+		}
+		catch (UnknownHostException ex_)
+		{
+			_logger.error(ex_.toString(), ex_);
+		}
+	}
 
 
 
 
 
-    public static final AppMetrics initFromConfig(XMLConfiguration cfg_)
-    {
+	public static final AppMetrics initFromConfig(String cfg_)
+	{
+		try
+		{
+			_logger.info("loading config from:{}", cfg_);
+			final Parameters params = new Parameters();
+			final FileBasedConfigurationBuilder<XMLConfiguration> builder = new FileBasedConfigurationBuilder<XMLConfiguration>(XMLConfiguration.class)
+					.configure(params.xml()
+							.setThrowExceptionOnMissing(false)
+							.setEncoding("UTF-8")
+							.setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
+							.setValidating(false)
+							.setFileName(cfg_));
+			final XMLConfiguration config = builder.getConfiguration();
+			return initFromConfig(config);
+		}
+		catch (ConfigurationException ex_)
+		{
+			_logger.error(ex_.toString(), ex_);
+		}
 
-        AppMetrics appM = getInstance();
+		return null;
+	}
 
-        appM.setEnabled(cfg_.getBoolean("AppMetrics.Enabled", true));
-        appM._registry.add(new SimpleMeterRegistry(SimpleConfig.DEFAULT, Clock.SYSTEM));
-        new TimedAspect(appM._registry);
-        if (!appM.isEnabled())
-        {
-            if (_logger.isDebugEnabled())
-            {
-                _logger.debug("application metrics are not enabled, no further configuration will be read/applied");
-            }
 
-            return appM;
-        }
 
-        appM.loadConfig(cfg_);
-        appM.configureRegistry();
 
 
-        return appM;
-    }
+	public static final AppMetrics initFromConfig(XMLConfiguration cfg_)
+	{
 
+		AppMetrics appM = getInstance();
 
+		appM.setEnabled(cfg_.getBoolean("AppMetrics.Enabled", true));
+		appM._registry.add(new SimpleMeterRegistry(SimpleConfig.DEFAULT, Clock.SYSTEM));
+		new TimedAspect(appM._registry);
+		if (!appM.isEnabled())
+		{
+			if (_logger.isDebugEnabled())
+			{
+				_logger.debug("application metrics are not enabled, no further configuration will be read/applied");
+			}
 
+			return appM;
+		}
 
+		appM.loadConfig(cfg_);
+		appM.configureRegistry();
 
-    public Timer createTimer(@NonNull String name_)
-    {
-        return createTimer(name_, null);
-    }
 
+		return appM;
+	}
 
 
 
 
-    public Timer createTimer(@NonNull String name_, String[] tags_)
-    {
-        if (_registry == null)
-        {
-            throw new RegistryNotReadyException("createTimer - registy not ready!");
-        }
 
-        if (_logger.isTraceEnabled())
-        {
-            _logger.trace("building timer:{}", name_);
-        }
+	public Timer createTimer(@NonNull String name_)
+	{
+		return createTimer(name_, null);
+	}
 
 
-        return Timer
-                .builder(name_)
-                .tags(tags_)
-                .description("a description of what this timer does") // optional
-                .publishPercentileHistogram()
-                .register(_registry);
-    }
 
 
 
+	public Timer createTimer(@NonNull String name_, String[] tags_)
+	{
+		if (_registry == null)
+		{
+			throw new RegistryNotReadyException("createTimer - registy not ready!");
+		}
 
+		if (_logger.isTraceEnabled())
+		{
+			_logger.trace("building timer:{}", name_);
+		}
 
-    public AtomicLong createGauge(@NonNull String name_)
-    {
-        if (_registry == null)
-        {
-            throw new RegistryNotReadyException("createTimer - registy not ready!");
-        }
 
-        if (_logger.isTraceEnabled())
-        {
-            _logger.trace("building timer:{}", name_);
-        }
+		return Timer
+				.builder(name_)
+				.tags(_commonTags)
+				.description("a description of what this timer does") // optional
+				.publishPercentileHistogram()
+				.register(_registry);
+	}
 
-        AtomicLong val = new AtomicLong(0);
-        _registry.gauge(name_, val);
-        return val;
-    }
 
 
 
 
+	public AtomicLong createGauge(@NonNull String name_)
+	{
+		if (_registry == null)
+		{
+			throw new RegistryNotReadyException("createTimer - registy not ready!");
+		}
 
-    public Counter createCounter(@NonNull String name_, String[] tags_)
-    {
-        if (_registry == null)
-        {
-            throw new RegistryNotReadyException("createTimer - registy not ready!");
-        }
+		if (_logger.isTraceEnabled())
+		{
+			_logger.trace("building timer:{}", name_);
+		}
 
-        if (_logger.isTraceEnabled())
-        {
-            _logger.trace("building timer:{}", name_);
-        }
+		AtomicLong val = new AtomicLong(0);
+		_registry.gauge(name_, _commonTags, val);
+		return val;
+	}
 
-        return _registry.counter(name_, tags_);
-    }
 
 
 
 
+	public Counter createCounter(@NonNull String name_, String[] tags_)
+	{
+		if (_registry == null)
+		{
+			throw new RegistryNotReadyException("createTimer - registy not ready!");
+		}
 
-    public Counter createCounter(@NonNull String name_)
-    {
-        return createCounter(name_, null);
-    }
+		if (_logger.isTraceEnabled())
+		{
+			_logger.trace("building timer:{}", name_);
+		}
 
+		return _registry.counter(name_, _commonTags);
+	}
 
 
 
 
-    private void loadConfig(XMLConfiguration cfg_)
-    {
-        _enabled = cfg_.getBoolean(buildKey("Enabled"), false);
-        if (!_enabled)
-        {
-            _logger.error("Metrics not enabled, config load abandoned");
-            return;
-        }
-        else
-        {
-            if (_logger.isTraceEnabled())
-            {
-                _logger.trace("Metrics enabled, config loading");
-            }
-        }
 
+	public Counter createCounter(@NonNull String name_)
+	{
+		return createCounter(name_, null);
+	}
 
-        _dbName = cfg_.getString(buildKey("DB.Name"));
-        boolean encryptedCreds = cfg_.getBoolean(buildKey("EncryptedCredentials"), false);
-        if (encryptedCreds)
-        {
-            _dbUsername = decrypt(cfg_, buildKey("DB.UserName"), "grafana");
-            _dbPassword = decrypt(cfg_, buildKey("DB.Pasword"), "grafana");
-        }
-        else
-        {
-            _dbUsername = cfg_.getString(buildKey("DB.UserName"), "grafana");
-            _dbPassword = cfg_.getString(buildKey("DB.Pasword"), "grafana");
-        }
 
-        _uri = cfg_.getString(buildKey("DB.URI"));
-        _appName = cfg_.getString(buildKey("AppName"), "AppName");
-        _autoCreateDb = cfg_.getBoolean(buildKey("AutoCreateDb"), true);
-        _batchSize = cfg_.getInt(buildKey("BatchSizing"), 1000);
-        _reportingFrequencyInSeconds = cfg_.getInt(buildKey("ReportingFrequencyInSeconds"), 5);
 
 
-        if (_logger.isInfoEnabled())
-        {
-            _logger.info("app-name:{}", _appName);
-            _logger.info("URI:{}", _uri);
-            _logger.info("database:{}", _dbName);
-        }
 
-    }
+	private void loadConfig(XMLConfiguration cfg_)
+	{
+		_enabled = cfg_.getBoolean(buildKey("Enabled"), false);
+		if (!_enabled)
+		{
+			_logger.error("Metrics not enabled, config load abandoned");
+			return;
+		}
+		else
+		{
+			if (_logger.isTraceEnabled())
+			{
+				_logger.trace("Metrics enabled, config loading");
+			}
+		}
 
 
+		_dbName = cfg_.getString(buildKey("DB.Name"));
+		_uri = cfg_.getString(buildKey("DB.URI"));
+		boolean encryptedCreds = cfg_.getBoolean(buildKey("EncryptedCredentials"), false);
+		if (encryptedCreds)
+		{
+			_dbUsername = decrypt(cfg_, buildKey("DB.UserName"), "grafana");
+			_dbPassword = decrypt(cfg_, buildKey("DB.Pasword"), "grafana");
+		}
+		else
+		{
+			_dbUsername = cfg_.getString(buildKey("DB.UserName"), "grafana");
+			_dbPassword = cfg_.getString(buildKey("DB.Pasword"), "grafana");
+		}
 
 
+		_appName = cfg_.getString(buildKey("AppName"), "AppName");
+		_autoCreateDb = cfg_.getBoolean(buildKey("AutoCreateDb"), true);
+		_batchSize = cfg_.getInt(buildKey("BatchSizing"), 1000);
+		_reportingFrequencyInSeconds = cfg_.getInt(buildKey("ReportingFrequencyInSeconds"), 5);
 
-    private String buildKey(@NonNull String key_)
-    {
-        String key = String.format("AppMetrics.%s", key_);
-        if (_logger.isTraceEnabled())
-        {
-            _logger.trace("key:{}", key);
-        }
-        return key;
-    }
 
+		if (_logger.isInfoEnabled())
+		{
+			_logger.info("app-name:{}", _appName);
+			_logger.info("URI:{}", _uri);
+			_logger.info("database:{}", _dbName);
+		}
 
+	}
 
 
 
-    private void configureRegistry()
-    {
-        if (!_enabled)
-        {
-            _logger.warn("influx metrics not enabled!");
-            return;
-        }
 
-        InfluxConfig cfg = new InfluxConfig()
-        {
-            @Override
-            public Duration step()
-            {
-                return Duration.ofSeconds(_reportingFrequencyInSeconds);
-            }
 
+	private String buildKey(@NonNull String key_)
+	{
+		String key = String.format("AppMetrics.%s", key_);
+		if (_logger.isTraceEnabled())
+		{
+			_logger.trace("key:{}", key);
+		}
+		return key;
+	}
 
 
 
 
-            @Override
-            public String db()
-            {
-                return _dbName;
-            }
 
+	private void configureRegistry()
+	{
+		if (!_enabled)
+		{
+			_logger.warn("influx metrics not enabled!");
+			return;
+		}
 
+		InfluxConfig cfg = new InfluxConfig()
+		{
+			@Override
+			public Duration step()
+			{
+				return Duration.ofSeconds(_reportingFrequencyInSeconds);
+			}
 
 
 
-            @Override
-            public String get(String key_)
-            {
-                return null;
-            }
 
 
+			@Override
+			public String db()
+			{
+				return _dbName;
+			}
 
 
 
-            @Override
-            public String uri()
-            {
-                return _uri;
-            }
 
 
-        };
+			@Override
+			public String get(String key_)
+			{
+				return null;
+			}
 
-        // # Whether to create the Influx database if it does not exist before attempting to publish metrics to it.
-        System.setProperty("management.metrics.export.influx.auto-create-db", Boolean.toString(_autoCreateDb));
 
-        // Number of measurements per request to use for this backend. If more measurements are found, then multiple requests will be made.
-        System.setProperty("management.metrics.export.influx.batch-size", Integer.toString(_batchSize));
 
-        // Whether exporting of metrics to this backend is enabled.
-        System.setProperty("management.metrics.export.influx.enabled", Boolean.toString(_enabled));
 
-        // Step size (i.e. reporting frequency) to use.
-        System.setProperty("management.metrics.export.influx.step", Integer.toString(_reportingFrequencyInSeconds) + "s");
 
-        // URI of the Influx server.
-        System.setProperty("management.metrics.export.influx.uri", _uri);
-        System.setProperty("management.metrics.export.influx.user-name", _dbUsername);
-        System.setProperty("management.metrics.export.influx.password", _dbPassword);
-        System.setProperty("management.metrics.export.influx.db", _dbName);
+			@Override
+			public String uri()
+			{
+				return _uri;
+			}
 
-        // Number of threads to use with the metrics publishing scheduler.
-        System.setProperty("management.metrics.export.influx.num-threads", "2");
 
-        // Read timeout for requests to this backend.
-        System.setProperty("management.metrics.export.influx.read-timeout", "10s");
+		};
 
-        // policy to use (Influx writes to the DEFAULT retention policy if one is not specified).
-        System.setProperty("management.metrics.export.influx.retention-policy", "Retention");
+		// # Whether to create the Influx database if it does not exist before attempting to publish metrics to it.
+		System.setProperty("management.metrics.export.influx.auto-create-db", Boolean.toString(_autoCreateDb));
 
-        // Whether to enable GZIP compression of metrics batches published to Influx.
-        System.setProperty("management.metrics.export.influx.compressed", "true");
+		// Number of measurements per request to use for this backend. If more measurements are found, then multiple requests will be made.
+		System.setProperty("management.metrics.export.influx.batch-size", Integer.toString(_batchSize));
 
-        // Connection timeout for requests to this backend.
-        System.setProperty("management.metrics.export.influx.connect-timeout", "1s");
+		// Whether exporting of metrics to this backend is enabled.
+		System.setProperty("management.metrics.export.influx.enabled", Boolean.toString(_enabled));
 
-        // Write consistency for each point.
-        System.setProperty("management.metrics.export.influx.consistency", "one");
+		// Step size (i.e. reporting frequency) to use.
+		System.setProperty("management.metrics.export.influx.step", Integer.toString(_reportingFrequencyInSeconds) + "s");
 
-        _registry.add(new InfluxMeterRegistry(cfg, Clock.SYSTEM));
+		// URI of the Influx server.
+		System.setProperty("management.metrics.export.influx.uri", _uri);
+		System.setProperty("management.metrics.export.influx.user-name", _dbUsername);
+		System.setProperty("management.metrics.export.influx.password", _dbPassword);
+		System.setProperty("management.metrics.export.influx.db", _dbName);
 
-        new JvmMemoryMetrics().bindTo(_registry);
-        new JvmGcMetrics().bindTo(_registry);
-        new ProcessorMetrics().bindTo(_registry);
-        new JvmThreadMetrics().bindTo(_registry);
-        new JvmCompilationMetrics().bindTo(_registry);
+		// Number of threads to use with the metrics publishing scheduler.
+		System.setProperty("management.metrics.export.influx.num-threads", "2");
 
-    }
+		// Read timeout for requests to this backend.
+		System.setProperty("management.metrics.export.influx.read-timeout", "10s");
 
+		// policy to use (Influx writes to the DEFAULT retention policy if one is not specified).
+		System.setProperty("management.metrics.export.influx.retention-policy", "Retention");
 
+		// Whether to enable GZIP compression of metrics batches published to Influx.
+		System.setProperty("management.metrics.export.influx.compressed", "true");
 
+		// Connection timeout for requests to this backend.
+		System.setProperty("management.metrics.export.influx.connect-timeout", "1s");
 
+		// Write consistency for each point.
+		System.setProperty("management.metrics.export.influx.consistency", "one");
 
-    public static final String decrypt(final XMLConfiguration cfg_, final String key_, String default_)
-    {
-        String encryptedString = cfg_.getString(key_);
-        if (StringUtils.isEmpty(encryptedString))
-        {
-            _logger.error("not found, key:{}, returning default:{}", key_, default_);
-            return default_;
-        }
+		_registry.add(new InfluxMeterRegistry(cfg, Clock.SYSTEM));
 
-        BasicTextEncryptor encryptor = new BasicTextEncryptor();
-        encryptor.setPassword(Long.toString(serialVersionUID));
-        return encryptor.decrypt(encryptedString);
-    }
+		new JvmMemoryMetrics().bindTo(_registry);
+		new JvmGcMetrics().bindTo(_registry);
+		new ProcessorMetrics().bindTo(_registry);
+		new JvmThreadMetrics().bindTo(_registry);
+		new JvmCompilationMetrics().bindTo(_registry);
 
+	}
 
 
 
 
-    ///////////////////////////////
-    //
-    ////////////////////////////////
-    public static final AppMetrics getInstance()
-    {
-        return SingletonAppMetrics._instance;
-    }
 
+	public static final String decrypt(final XMLConfiguration cfg_, final String key_, String default_)
+	{
+		String encryptedString = cfg_.getString(key_);
+		if (StringUtils.isEmpty(encryptedString))
+		{
+			_logger.error("not found, key:{}, returning default:{}", key_, default_);
+			return default_;
+		}
 
+		BasicTextEncryptor encryptor = new BasicTextEncryptor();
+		encryptor.setPassword(Long.toString(serialVersionUID));
+		return encryptor.decrypt(encryptedString);
+	}
 
 
 
-    private static final class SingletonAppMetrics
-    {
-        private static final AppMetrics _instance = new AppMetrics();
-    }
+
+
+	///////////////////////////////
+	//
+	////////////////////////////////
+	public static final AppMetrics getInstance()
+	{
+		return SingletonAppMetrics._instance;
+	}
+
+
+
+
+
+	private static final class SingletonAppMetrics
+	{
+		private static final AppMetrics _instance = new AppMetrics();
+	}
 
 
 
